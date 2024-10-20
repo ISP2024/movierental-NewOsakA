bae9d63124491f19e17c223588af99aabbcd597f
NewOsakA <115053961+NewOsakA@users.noreply.github.com>
2024-10-20 16:53:45 +0700

    Add Attributes to Movie

--- movie.py
+++ movie.py
@@ -1,26 +1,20 @@
 from pricing import NEW_RELEASE, REGULAR, CHILDREN
+from dataclasses import dataclass
+from typing import Collection
 
 
+@dataclass(frozen=True)
 class Movie:
     """
     A movie available for rent.
     """
-    # The types of movies (price_code). 
-    NEW_RELEASE = NEW_RELEASE
-    REGULAR = REGULAR
-    CHILDRENS = CHILDREN
-    
-    def __init__(self, title, price_code):
-        # Initialize a new movie. 
-        self.title = title
-        self.price_code = price_code
 
-    def get_price_code(self):
-        # get the price code
-        return self.price_code
-    
-    def get_title(self):
-        return self.title
+    title: str
+    year: int
+    genre: Collection[str]
+
+    def is_genre(self, genre: str):
+        return genre.lower() in [single_genre.lower() for single_genre in self.genre]
     
     def __str__(self):
-        return self.title
+        return f"{self.title} ({self.year})"
